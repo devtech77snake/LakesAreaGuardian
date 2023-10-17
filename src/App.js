ea6c1e4a8cad6c8a83fbd78cf70ce15c2f259f22
Bolt-Shiny <Bolt.Shiny@outlook.com>
2023-10-17 06:02:33 -0400

    #11 update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
